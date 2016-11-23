b89e015c22e5b047c2c1543c010e358a01f10889
Xiaolei2016 <xguo2016@fau.edu>
2016-11-22 20:12:14 -0500

    update sql

--- WebContent/WEB-INF/descr/sql/data.sql
+++ WebContent/WEB-INF/descr/sql/data.sql
@@ -3,32 +3,32 @@
 -- -------- POPULATE DATABASE ---------------------------------------------------------------------------------------------
 
 -- ---------- POPUPLATE TABLE `MODULE` ----------
-INSERT INTO "APP"."MODULE" ("oid", "MODULE_ID", "MODULE_NAME") VALUES (1, 'sv3', 'Administrator');
-INSERT INTO "APP"."MODULE" ("oid", "MODULE_ID", "MODULE_NAME") VALUES (2, 'sv1', 'eShop');
+INSERT INTO `module` (`oid`, `MODULEID`, `MODULENAME`) VALUES (1, 'sv3', 'admin');
+INSERT INTO `module` (`oid`, `MODULEID`, `MODULENAME`) VALUES (2, 'sv1', 'eBookStore');
 
--- ---------- POPUPLATE TABLE "GROUP" ----------
-INSERT INTO "APP"."GROUP" ("oid", "GROUP_NAME", "MODULE_OID") VALUES (1, 'Administrators', 1);
-INSERT INTO "APP"."GROUP" ("oid", "GROUP_NAME", "MODULE_OID") VALUES (2, 'Customers', 2);
+-- ---------- POPUPLATE TABLE `GROUP` ----------
+INSERT INTO `group` (`oid`, `GROUPNAME`, `MODULE_OID`) VALUES (1, 'admin', 1);
+INSERT INTO `group` (`oid`, `GROUPNAME`, `MODULE_OID`) VALUES (2, 'Customers', 2);
 
--- ---------- POPUPLATE TABLE "USER" ----------
-INSERT INTO "APP"."USER" ("oid", "USERNAME", "PASSWORD", "EMAIL", "GROUP_OID") VALUES (1, 'manager', 'manager', 'manager@gmail.com', 1);
+-- ---------- POPUPLATE TABLE `USER` ----------
+INSERT INTO `user` (`oid`, `USERNAME`, `PASSWORD`, `EMAIL`, `GROUP_OID`) VALUES (1, 'manager', 'manager', 'manager@gmail.com', 1);
 
--- ---------- POPUPLATE TABLE "USER_GROUP" ----------
-INSERT INTO "APP"."USER_GROUP" ("USER_OID", "GROUP_OID") VALUES (1, 1);
-INSERT INTO "APP"."USER_GROUP" ("USER_OID", "GROUP_OID") VALUES (1, 2);
+-- ---------- POPUPLATE TABLE `USER_GROUP` ----------
+INSERT INTO `user_group` (`USER_OID`, `GROUP_OID`) VALUES (1, 1);
+INSERT INTO `user_group` (`USER_OID`, `GROUP_OID`) VALUES (1, 2);
 
--- ---------- POPUPLATE TABLE "GROUP_MODULE" ----------
-INSERT INTO "APP"."GROUP_MODULE" ("GROUP_OID", "MODULE_OID") VALUES (1, 1);
-INSERT INTO "APP"."GROUP_MODULE" ("GROUP_OID", "MODULE_OID") VALUES (1, 2);
-INSERT INTO "APP"."GROUP_MODULE" ("GROUP_OID", "MODULE_OID") VALUES (2, 2);
+-- ---------- POPUPLATE TABLE `GROUP_MODULE` ----------
+INSERT INTO `group_module` (`GROUP_OID`, `MODULE_OID`) VALUES (1, 1);
+INSERT INTO `group_module` (`GROUP_OID`, `MODULE_OID`) VALUES (1, 2);
+INSERT INTO `group_module` (`GROUP_OID`, `MODULE_OID`) VALUES (2, 2);
 
 
--- ---------- POPUPLATE TABLE "category" ----------
-INSERT INTO "APP"."category" ("oid", "category") VALUES (1, 'Science fiction');
-INSERT INTO "APP"."category" ("oid", "category") VALUES (2, 'Drama');
-INSERT INTO "APP"."category" ("oid", "category") VALUES (3, 'Mystery');
-INSERT INTO "APP"."category" ("oid", "category") VALUES (4, 'Action and Adventure');
-INSERT INTO "APP"."category" ("oid", "category") VALUES (5, 'Horror');
+-- ---------- POPUPLATE TABLE `category` ----------
+INSERT INTO `category` (`oid`, `category`) VALUES (1, 'Science fiction');
+INSERT INTO `category` (`oid`, `category`) VALUES (2, 'Drama');
+INSERT INTO `category` (`oid`, `category`) VALUES (3, 'Mystery');
+INSERT INTO `category` (`oid`, `category`) VALUES (4, 'Action and Adventure');
+INSERT INTO `category` (`oid`, `category`) VALUES (5, 'Horror');
 
 -- ---------- POPUPLATE TABLE "BOOKINFO" ----------
 INSERT INTO "APP"."BOOKINFO" ("oid", "isbn", "description", "name", "price", "photo", "loginprice", "publisher", "author", "issale", "isrent", "category_oid", "category") VALUES (1, 9787560542966, 'DESCRIPTION for category 1.', 'Science fiction name 1', 39, 'upload/small_chair_5.jpg', 35, 'publisher 1', 'author 1', true, true, 1, 'Science fiction');
