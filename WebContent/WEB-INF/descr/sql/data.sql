778f2f1dcf2b408f5011196e0af75d6b66468282
Xiaolei2016 <xguo2016@fau.edu>
2016-11-29 14:27:55 -0500

    update sql

--- WebContent/WEB-INF/descr/sql/data.sql
+++ WebContent/WEB-INF/descr/sql/data.sql
@@ -4,23 +4,18 @@
 
 -- ---------- POPUPLATE TABLE `MODULE` ----------
 INSERT INTO `module` (`oid`, `MODULEID`, `MODULENAME`) VALUES (1, 'sv3', 'admin');
-INSERT INTO `module` (`oid`, `MODULEID`, `MODULENAME`) VALUES (2, 'sv1', 'eBookStore');
+INSERT INTO `module` (`oid`, `MODULEID`, `MODULENAME`) VALUES (2, 'sv1', 'member');
 
 -- ---------- POPUPLATE TABLE `GROUP` ----------
 INSERT INTO `group` (`oid`, `GROUPNAME`, `MODULE_OID`) VALUES (1, 'admin', 1);
-INSERT INTO `group` (`oid`, `GROUPNAME`, `MODULE_OID`) VALUES (2, 'Customers', 2);
+INSERT INTO `group` (`oid`, `GROUPNAME`, `MODULE_OID`) VALUES (2, 'member', 2);
 
 -- ---------- POPUPLATE TABLE `USER` ----------
 INSERT INTO `user` (`oid`, `USERNAME`, `PASSWORD`, `EMAIL`, `GROUP_OID`) VALUES (1, 'manager', 'manager', 'manager@gmail.com', 1);
 
 -- ---------- POPUPLATE TABLE `USER_GROUP` ----------
 INSERT INTO `user_group` (`USER_OID`, `GROUP_OID`) VALUES (1, 1);
-INSERT INTO `user_group` (`USER_OID`, `GROUP_OID`) VALUES (1, 2);
-
--- ---------- POPUPLATE TABLE `GROUP_MODULE` ----------
-INSERT INTO `group_module` (`GROUP_OID`, `MODULE_OID`) VALUES (1, 1);
-INSERT INTO `group_module` (`GROUP_OID`, `MODULE_OID`) VALUES (1, 2);
-INSERT INTO `group_module` (`GROUP_OID`, `MODULE_OID`) VALUES (2, 2);
+INSERT INTO `user_group` (`USER_OID`, `GROUP_OID`) VALUES (2, 2);
 
 
 -- ---------- POPUPLATE TABLE `category` ----------
